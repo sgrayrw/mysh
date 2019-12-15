065e2a00149f49bef40931cd18b807c0e3d1d465
Ruikang Shi <rshi1@haverford.edu>
2019-12-15 12:51:23 -0500

    update

--- src/fs/fs.c
+++ src/fs/fs.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/time.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include "error.h"
 #include "fs.h"
 #include "../shell/mysh.h"
@@ -63,6 +64,7 @@ int f_open(const char* pathname, const char* mode) {
                 error = PERM_DENIED;
                 return FAILURE;
             }
+            printf("start!\n");
             vnode = create_file(parentdir, path[length - 1], F, "rw--");
             if (!vnode) {
                 error = DISK_FULL;
@@ -717,12 +719,13 @@ int readdir(vnode_t* dir, int n, dirent_t* dirent, inode_t* inode) {
     inode_t dir_inode;
     fetch_inode(dir, &dir_inode);
 
-    if (n > dir_inode.size)
+    if (n >= dir_inode.size)
         return FAILURE;
 
     FILE* disk = disks[dir->disk];
     long long block_number = n / 2 + 1;
     long long block_addr = get_block_address(dir, block_number);
+    printf("block_addr%d\n",block_addr);
     fseek(disk, block_addr + (BLOCKSIZE * (n % 2)), SEEK_SET);
     fread(dirent, sizeof(dirent_t), 1, disk);
     if (dirent->type != EMPTY) {
@@ -736,13 +739,11 @@ int readdir(vnode_t* dir, int n, dirent_t* dirent, inode_t* inode) {
 long long get_block(int n_disk) {
     FILE* disk = disks[n_disk];
     sb_t* sb = superblocks[n_disk];
-
     if (sb->free_block == -1)
         return FAILURE;
 
     // get next free block
     long long free_block = sb->free_block;
-
     // update free block list in superblock
     fseek(disk, free_block, SEEK_SET);
     fread(&(sb->free_block), sizeof(long long), 1, disk);
@@ -750,7 +751,6 @@ long long get_block(int n_disk) {
     // update superblock in disk
     fseek(disk, BOOTSIZE, SEEK_SET);
     fwrite(sb, sizeof(sb_t), 1, disk);
-
     return free_block;
 }
 
@@ -918,12 +918,11 @@ long long get_block_address(vnode_t* vnode, long long block_number){
     FILE* fs = disks[vnode->disk];
     inode_t inode;
     fetch_inode(vnode, &inode);
-    long long totalblock = (inode.size-1)/BLOCKSIZE+1;
+    long long totalblock = inode.size == 0 ? 0 : (inode.size-1)/BLOCKSIZE+1;
     bool new = false;
     if (block_number>totalblock){
         new = true;
     }
-
     int index[6];
     if (get_block_index(block_number,index)==FAILURE){
         return FAILURE;
@@ -972,7 +971,8 @@ long long get_block_address(vnode_t* vnode, long long block_number){
     fwrite(&inode,sizeof(inode_t),1,fs);
     if (new){
         if (index[0] == 0){
-            if ((newaddress = get_block(vnode->disk)==FAILURE)){
+            printf("here/n");
+            if ((newaddress=get_block(vnode->disk)) ==FAILURE){
                 return FAILURE;
             }
             inode.dblocks[index[1]] = newaddress;
