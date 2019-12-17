d913958a433f7299cb43e8f416e37deb6372ec9f
Yuxiao Wang <me@rayrw.com>
2019-12-16 22:13:50 -0800

    update

--- src/fs/test.c
+++ src/fs/test.c
@@ -8,26 +8,27 @@ int user_id = ID_SUPERUSER;
 int main() {
     init();
     f_mount("DISK", "/");
+    set_wd("/");
     int f = f_open("/file", "w");
     f_close(f);
-
-    f_mkdir("/dir", "rw--", false);
-    f = f_opendir("/dir");
-    f_closedir(f);
-    f = f_open("/dir/tests", "w");
-    char* buffer = "abcdefg";
-    int fee = f_write(f,(void*)buffer,5);
-    printf("%d\n",fee);
-    f_close(f);
-    f = f_open("/dir/tests", "r");
-    fee = f_seek(f,2,SEEK_SET);
-    char* buffer2[10];
-    printf("%d,error:%d\n",fee,error);
-    fee = f_read(f,(void*)buffer2,4);
-    buffer2[7] = "\0";
-    printf("%d,error:%d\n",fee,error);
-    printf("%s\n",buffer2);
-    f_close(f);
+//
+//    f_mkdir("/dir", "rw--", false);
+//    f = f_opendir("/dir");
+//    f_closedir(f);
+//    f = f_open("/dir/tests", "w");
+//    char* buffer = "abcdefg";
+//    int fee = f_write(f,(void*)buffer,5);
+//    printf("%d\n",fee);
+//    f_close(f);
+//    f = f_open("/dir/tests", "r");
+//    fee = f_seek(f,2,SEEK_SET);
+//    char* buffer2[10];
+//    printf("%d,error:%d\n",fee,error);
+//    fee = f_read(f,(void*)buffer2,4);
+//    buffer2[7] = "\0";
+//    printf("%d,error:%d\n",fee,error);
+//    printf("%s\n",buffer2);
+//    f_close(f);
 
     dump();
     term();
