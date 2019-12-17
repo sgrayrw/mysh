aaddab319c6e57453d2454a7d40e6f757eb20d78
Yuxiao Wang <me@rayrw.com>
2019-12-16 16:22:44 -0800

    update

--- src/fs/test.c
+++ src/fs/test.c
@@ -8,10 +8,17 @@ int user_id = ID_SUPERUSER;
 int main() {
     init();
     f_mount("DISK", "/");
-    f_open("/ray", "w");
-    f_open("/ruikang", "w");
-    f_open("/jiyu", "w");
-    f_open("/dxu", "w");
+    int f = f_open("/ray", "w");
+    char* str = "hello";
+    f_write(f, str, 6);
+    f_close(f);
+
+    f = f_open("/ray", "r");
+    char str2[6];
+    f_read(f, str2, 5);
+    str2[5] = '\0';
+    printf("%s\n", str2);
+
     dump();
     term();
 }
